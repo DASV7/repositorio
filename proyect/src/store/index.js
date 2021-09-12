c43fd0993dec650f913b039218977bba7d6d8b54
anti22222 <84532486+anti22222@users.noreply.github.com>
2021-09-12 14:19:26 -0500

    Create app

---
+++ proyect/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    //return data of api
+    api_complete: [],
+  },
+  mutations: {
+    
+
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
